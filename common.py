59c1d6d1eab6a66c16980618cf18fbdfebf08574
leizhihao <leizhihao@iqianjin.com>
2017-12-26 02:46:08 +0800

    add comments

--- common.py
+++ common.py
@@ -1,11 +1,13 @@
 import json
 import media
 
+# read a json file and return data of dict type
 def open_json(file_str):
 	with open(file_str) as json_file:
 		data = json.load(json_file)
 		return data
 
+# transform a dict type argument to a list within Movie instances
 def dict_to_movies(data): 
 	movies = [];
 	for i in data:
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,11 @@
 import fresh_tomatoes
 import common
 
+# get data
 datas = common.open_json('data.json');
 
+# transform data to movies
 movies = common.dict_to_movies(datas)
 
+# get html
 fresh_tomatoes.open_movies_page(movies);
